1964862438325e10123caf8f2decad922b4c57d6
Serhat Oğuz <sserhatoguz@outlook.com>
2022-10-03 21:33:22 +0300

    Add project files.

---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi, sonuc = 1;
+            sayi = Convert.ToInt32(txtSayi.Text);
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                sonuc *= i;
+            }
+            lblSonuc.Text = sonuc.ToString();
+        }
+    }
+}
